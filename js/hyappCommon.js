f57986845b055c5f8ad8800ddfbc95f9c4f61131
holagoldfish <hola_goldfish@163.com>
2016-11-20 17:08:42 +0800

    h5app

---
+++ js/hyappCommon.js
@@ -0,0 +1,33 @@
+/*var ServerUrl={
+	"IP":"http://192.168.51.109:8081"
+};
+*/
+
+/*var ServerUrl={
+	"IP":"http://192.168.1.112:8081"
+};
+*/
+
+/*var ServerUrl={
+	"IP":"http://192.168.51.111:8081"
+};
+
+var websocketUrl={
+	"IP":"ws://192.168.51.111:8081"
+};
+*/
+
+/*var ServerUrl={
+	"IP":"http://15966o51e3.iask.in:80"
+};*/
+
+
+//192.168.43.203
+
+var ServerUrl={
+	"IP":"http://192.168.43.203:8081"
+};
+
+var websocketUrl={
+	"IP":"ws://192.168.43.203:8081"
+};
